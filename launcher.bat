4244c930fa375f73be264ad8576dda89210767ec
DoppiePoppie <70096724+DoppiePoppie@users.noreply.github.com>
2021-07-22 01:25:53 +0200

    Add files via upload

--- launcher.bat
+++ launcher.bat
@@ -1,36 +1,41 @@
 @ECHO OFF
-ECHO Currently running launcher version 0.1. A newer one may be available on https://github.com/DoppiePoppie/MommyMiner/releases/latest/
+ECHO Currently running launcher version 0.3. Downloaded from: https://github.com/DoppiePoppie/MommyMiner/releases/latest/
+ECHO Before closing the miner, make sure to save the logs to verify with Mommy later on.
+ECHO Make sure to add the folder to exceptions in your antivirus settings.
+ECHO Otherwise, the downloaded file may get flagged and removed before you know it.
 ECHO Checking for autorun settings.
 IF EXIST "%cd%\settings.doppel" (
   ECHO Found autorun settings.
-  ECHO Input Y within 10 seconds to go to settings.
-  ECHO Input N or wait 10 seconds to autorun.
-  CHOICE /T 10 /C YN /D N
-  IF %ERRORLEVEL%==1 (
-    GOTO DOWNLOAD
-  )
-  IF %ERRORLEVEL%==2 (
-    GOTO AUTORUN
-  )
+  GOTO AUTORUNWAIT
 ) ELSE (
-  ECHO No autorun settings found. Forwarding to download.
+  ECHO No autorun settings found. Forwarding to the menu.
   GOTO DOWNLOAD
+)
+
+:AUTORUNWAIT
+ECHO Input Y within 10 seconds to go to settings.
+ECHO Input N or wait 10 seconds to autorun.
+CHOICE /T 10 /C YN /D N
+IF %ERRORLEVEL%==1 (
+  GOTO DOWNLOAD
+)
+IF %ERRORLEVEL%==2 (
+  GOTO AUTORUN
 )
 
 :AUTORUN
 ECHO Searching for executable.
-IF EXIST "%cd%\nsfminer.exe" (
+IF EXIST "%cd%\data\miner.exe" (
   ECHO Executable exists, attempting autorun.
   GOTO RUN
 ) ELSE (
-  ECHO Despite autorun settings, the executable doesn't exist. Forwarding to download and resetting autorun settings.
+  ECHO Despite autorun settings, the executable doesn't exist. Forwarding to the menu and resetting autorun settings.
   DEL settings.doppel
   IF EXIST "%cd%\nosettings.doppel" (
     DEL nosettings.doppel
   )
   GOTO DOWNLOAD
 )
-
 
 :DOWNLOAD
 IF EXIST "%cd%\settings.doppel" (
@@ -43,70 +48,41 @@ IF EXIST "%cd%\nosettings.doppel" (
 ) ELSE (
   SET NOAUTORUNSETTING=OFF
 )
-IF NOT EXIST "%cd%\nsfminer.exe" (
-  SET NOMINER=Miner not found.
+IF NOT EXIST "%cd%\data\miner.exe" (
+  SET NOMINER=Miner not found, download it first.
 ) ELSE (
   SET NOMINER=Miner found.
 )
+IF EXIST "%cd%\workername.doppel" (
+  CALL :READUSERNAME
+) ELSE (
+  SET MENUWORKERNAME=No name set.
+)
 ECHO -------------------------------------------------------------------------------------
-ECHO 1 - Download for NVidia GPUs (CUDA).
-ECHO 2 - Download for AMD GPUs (OpenCL).
-ECHO 3 - Download for both GPUs (CUDA and OpenCL).
-ECHO 4 - Switch autorun setting. (Currently: %AUTORUNSETTING%)
-ECHO 5 - Switch autorun question setting. (Currently: %NOAUTORUNSETTING%)
-ECHO 6 - Attempt to run the miner. (%NOMINER%)
+ECHO 1 - Download the miner.
+ECHO 2 - Switch autorun setting. (Currently: %AUTORUNSETTING%)
+ECHO 3 - Change workername. (%MENUWORKERNAME%)
+ECHO 4 - Attempt to run the miner. (%NOMINER%)
 ECHO -------------------------------------------------------------------------------------
 ECHO.
-SET /P M=Type 1, 2, 3, 4, 5, or 6, then press ENTER:
-IF %M%==1 GOTO CUDA
-IF %M%==2 GOTO OPENCL
-IF %M%==3 GOTO BOTH
-IF %M%==4 GOTO SWITCHAUTORUN
-IF %M%==5 GOTO SWITCHNOAUTORUN
-IF %M%==6 GOTO RUN
+SET /P M=Type 1, 2, 3, or 4, then press ENTER:
+IF %M%==1 GOTO ACTUALDOWNLOAD
+IF %M%==2 GOTO SWITCHAUTORUN
+IF %M%==3 GOTO CHANGEUSERNAME
+IF %M%==4 GOTO RUN
 
-:CUDA
+:ACTUALDOWNLOAD
 ECHO.
 ECHO Downloading the CUDA miner, please wait...
-%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/no-fee-ethereum-mining/nsfminer/releases/download/v1.3.14/nsfminer_1.3.14-windows_10-cuda_11.3.zip "%cd%\package.zip"
-GOTO UNPACK
-
-:OPENCL
-ECHO.
-ECHO Downloading the OpenCL miner, please wait...
-%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/no-fee-ethereum-mining/nsfminer/releases/download/v1.3.14/nsfminer_1.3.14-windows_10-opencl.zip "%cd%\package.zip"
-GOTO UNPACK
-
-:BOTH
-ECHO.
-ECHO Downloading the universal miner, please wait...
-%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/no-fee-ethereum-mining/nsfminer/releases/download/v1.3.14/nsfminer_1.3.14-windows_10-cuda_11.3-opencl.zip "%cd%\package.zip"
+%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/develsoftware/GMinerRelease/releases/download/2.62/gminer_2_62_windows64.zip "%cd%\package.zip"
 GOTO UNPACK
 
 :UNPACK
 ECHO.
 ECHO Unpacking the downloaded file, please wait...
-powershell -Command "Expand-Archive package.zip -DestinationPath %cd%\\ -Force"
-GOTO SETUPAUTORUN
-
-:SETUPAUTORUN
-IF IF EXIST "%cd%\nosettings.doppel" (
-  GOTO RUN
-) ELSE (
-  ECHO.
-  ECHO -------------------------------------------------------------------------------------
-  ECHO 1 - Save these settings, enabling the script to instantly run the set-up profile.
-  ECHO 2 - Don't save the settings, let me redownload next time.
-  ECHO -------------------------------------------------------------------------------------
-  ECHO.
-  SET /P M=Type 1 or 2, then press ENTER:
-  IF %M%==1 GOTO CREATEAUTORUN
-  IF %M%==2 GOTO AUTORUNQUESTION
-)
-
-:CREATEAUTORUN
-copy /y NUL settings.doppel >NUL
-GOTO RUN
+IF NOT EXIST "%cd%\data" mkdir %cd%\data
+powershell -Command "Expand-Archive package.zip -DestinationPath %cd%\\data\\ -Force"
+GOTO DOWNLOAD
 
 :SWITCHAUTORUN
 IF EXIST "%cd%\settings.doppel" (
@@ -115,21 +91,6 @@ IF EXIST "%cd%\settings.doppel" (
   copy /y NUL settings.doppel >NUL
 )
 GOTO DOWNLOAD
-
-:AUTORUNQUESTION
-ECHO.
-ECHO -------------------------------------------------------------------------------------
-ECHO 1 - Don't ask me about autorun options again.
-ECHO 2 - Do ask me about autorun again next time.
-ECHO -------------------------------------------------------------------------------------
-ECHO.
-SET /P M=Type 1 or 2, then press ENTER:
-IF %M%==1 GOTO NOMOREAUTORUN
-IF %M%==2 GOTO RUN
-
-:NOMOREAUTORUN
-copy /y NUL nosettings.doppel >NUL
-GOTO RUN
 
 :SWITCHNOAUTORUN
 IF EXIST "%cd%\nosettings.doppel" (
@@ -140,8 +101,40 @@ IF EXIST "%cd%\nosettings.doppel" (
 GOTO DOWNLOAD
 
 :RUN
-ECHO Attempting to run the executable.
+IF NOT EXIST "%cd%\data\miner.exe" (
+  ECHO Miner not found. Make sure to download the miner beforehand and to add the folder as an exception in your antivirus.
+  ECHO Returning to the menu.
+)
+IF EXIST "%cd%\workername.doppel" (
+  for /F "tokens=*" %%a in (%cd%\workername.doppel) do (
+    ECHO Attempting to run the executable.
+    ECHO Beginning the mining process under workername (%%a).
+    .\data\miner.exe --algo ethash --server eth.2miners.com:2020 --user 0xb5c174e8061d8b4d355b5b1f64dd63f0f18c9deb.%%a
+  )
+) ELSE (
+  GOTO SETUSERNAME
+)
+PAUSE
 GOTO ENDOFFILE
+
+:SETUSERNAME
+ECHO No workername found set.
+ECHO Please set a workername that will be definitely identifiable. Feel free to use a Twitter handle (@Handle), Discord tag (YourTag#XXXX) or anything else that will surely lead Mommy to you for easy verification.
+ECHO Choosing a workername that does not distinguish you from other workers may lead to you being unable to receive compensation for your mining.
+SET /P id=Enter your workername:
+echo %id% > workername.doppel
+GOTO RUN
+
+:READUSERNAME
+for /F "tokens=*" %%b in (%cd%\workername.doppel) do (
+  SET MENUWORKERNAME=%%b
+)
+GOTO :EOF
+
+:CHANGEUSERNAME
+SET /P id=Enter your new workername:
+echo %id%> workername.doppel
+GOTO DOWNLOAD
 
 :ENDOFFILE
 PAUSE
